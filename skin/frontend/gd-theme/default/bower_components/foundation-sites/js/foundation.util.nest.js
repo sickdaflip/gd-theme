dc800c152949bef188a6dd2c89cece8aa84d6476
Froxlor Customer <magento@dev.gastro.lan>
2017-07-05 15:20:09 +0200

    cleanup Theme

--- skin/frontend/gd-theme/default/bower_components/foundation-sites/js/foundation.util.nest.js
+++ skin/frontend/gd-theme/default/bower_components/foundation-sites/js/foundation.util.nest.js
@@ -2,75 +2,83 @@
 
 !function($) {
 
-    const Nest = {
-        Feather(menu, type = 'zf') {
-            menu.attr('role', 'menubar');
+const Nest = {
+  Feather(menu, type = 'zf') {
+    menu.attr('role', 'menubar');
 
-            var items = menu.find('li').attr({'role': 'menuitem'}),
-                subMenuClass = `is-${type}-submenu`,
-                subItemClass = `${subMenuClass}-item`,
-                hasSubClass = `is-${type}-submenu-parent`,
-                applyAria = (type !== 'accordion'); // Accordions handle their own ARIA attriutes.
+    var items = menu.find('li').attr({'role': 'menuitem'}),
+        subMenuClass = `is-${type}-submenu`,
+        subItemClass = `${subMenuClass}-item`,
+        hasSubClass = `is-${type}-submenu-parent`,
+        applyAria = (type !== 'accordion');
 
-            menu.find('a:first').attr('tabindex', 0);
+    items.each(function() {
+      var $item = $(this),
+          $sub = $item.children('ul');
 
-            items.each(function() {
-                var $item = $(this),
-                    $sub = $item.children('ul');
+      if ($sub.length) {
+          $item.addClass(hasSubClass);
+                  $sub.addClass(`submenu ${subMenuClass}`).attr({'data-submenu': ''});
+                  if(applyAria) {
+                      $item.attr({
+                          'aria-haspopup': true,
+                          'aria-label': $item.children('a:first').text()
+                      });
+                      // Note:  Drilldowns behave differently in how they hide, and so need
+                      // additional attributes.  We should look if this possibly over-generalized
+                      // utility (Nest) is appropriate when we rework menus in 6.4
+                  }
+          if(type === 'drilldown') {
+            $item.attr({'aria-expanded': false});
+          }
 
-                if ($sub.length) {
-                    $item.addClass(hasSubClass);
-                    $sub.addClass(`submenu ${subMenuClass}`).attr({'data-submenu': ''});
-                    if(applyAria) {
-                        $item.attr({
-                            'aria-haspopup': true,
-                            'aria-expanded': false,
-                            'aria-label': $item.children('a:first').text()
-                        });
-
-                        $sub.attr({
-                            'aria-hidden': true,
-                            'role': 'menu'
-                        });
-                    }
-                }
-
-                if ($item.parent('[data-submenu]').length) {
-                    $item.addClass(`is-submenu-item ${subItemClass}`);
-                }
-            });
-
-            return;
-        },
-
-        Burn(menu, type) {
-            var items = menu.find('li').removeAttr('tabindex'),
-                subMenuClass = `is-${type}-submenu`,
-                subItemClass = `${subMenuClass}-item`,
-                hasSubClass = `is-${type}-submenu-parent`;
-
-            menu
-                .find('>li, .menu, .menu > li')
-                .removeClass(`${subMenuClass} ${subItemClass} ${hasSubClass} is-submenu-item submenu is-active`)
-                .removeAttr('data-submenu').css('display', '');
-
-            // console.log(      menu.find('.' + subMenuClass + ', .' + subItemClass + ', .has-submenu, .is-submenu-item, .submenu, [data-submenu]')
-            //           .removeClass(subMenuClass + ' ' + subItemClass + ' has-submenu is-submenu-item submenu')
-            //           .removeAttr('data-submenu'));
-            // items.each(function(){
-            //   var $item = $(this),
-            //       $sub = $item.children('ul');
-            //   if($item.parent('[data-submenu]').length){
-            //     $item.removeClass('is-submenu-item ' + subItemClass);
-            //   }
-            //   if($sub.length){
-            //     $item.removeClass('has-submenu');
-            //     $sub.removeClass('submenu ' + subMenuClass).removeAttr('data-submenu');
-            //   }
-            // });
+        $sub
+          .addClass(`submenu ${subMenuClass}`)
+          .attr({
+            'data-submenu': '',
+            'role': 'menu'
+          });
+        if(type === 'drilldown') {
+          $sub.attr({'aria-hidden': true});
         }
-    }
+      }
 
-    Foundation.Nest = Nest;
+      if ($item.parent('[data-submenu]').length) {
+        $item.addClass(`is-submenu-item ${subItemClass}`);
+      }
+    });
+
+    return;
+  },
+
+  Burn(menu, type) {
+    var //items = menu.find('li'),
+        subMenuClass = `is-${type}-submenu`,
+        subItemClass = `${subMenuClass}-item`,
+        hasSubClass = `is-${type}-submenu-parent`;
+
+    menu
+      .find('>li, .menu, .menu > li')
+      .removeClass(`${subMenuClass} ${subItemClass} ${hasSubClass} is-submenu-item submenu is-active`)
+      .removeAttr('data-submenu').css('display', '');
+
+    // console.log(      menu.find('.' + subMenuClass + ', .' + subItemClass + ', .has-submenu, .is-submenu-item, .submenu, [data-submenu]')
+    //           .removeClass(subMenuClass + ' ' + subItemClass + ' has-submenu is-submenu-item submenu')
+    //           .removeAttr('data-submenu'));
+    // items.each(function(){
+    //   var $item = $(this),
+    //       $sub = $item.children('ul');
+    //   if($item.parent('[data-submenu]').length){
+    //     $item.removeClass('is-submenu-item ' + subItemClass);
+    //   }
+    //   if($sub.length){
+    //     $item.removeClass('has-submenu');
+    //     $sub.removeClass('submenu ' + subMenuClass).removeAttr('data-submenu');
+    //   }
+    // });
+  }
+}
+
+Foundation.Nest = Nest;
 
 }(jQuery);
